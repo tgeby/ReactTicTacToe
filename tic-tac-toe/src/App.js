dd330c5072640d4c2728b62bd86d1884e2f0b128
Thomas Eby <tgeby@icloud.com>
2025-06-17 15:13:50 -0400

    Squares use state variables and can be set to Xs

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function Square({ value }) {
+function Square() {
+
+  const [value, setValue] = useState(null);
+
   function handleClick() {
-    console.log("clicked!");
+    setValue('X');
   }
   
   return (
@@ -20,19 +24,19 @@ function Board() {
   return (
   <>
     <div className="board-row">
-      <Square value="1"/>
-      <Square value="2"/>
-      <Square value="3"/>
+      <Square />
+      <Square />
+      <Square />
     </div>
     <div className="board-row">
-      <Square value="4"/>
-      <Square value="5"/>
-      <Square value="6"/>
+      <Square />
+      <Square />
+      <Square />
     </div>
     <div className="board-row">
-      <Square value="7"/>
-      <Square value="8"/>
-      <Square value="9"/>
+      <Square />
+      <Square />
+      <Square />
     </div>
   </>
   );
